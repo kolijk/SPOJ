86263bab18e00ac51454fd5a54755d5104d9ab58
kolijk <kolijk.ggftd@gmail.com>
2026-04-19 11:39:19 +0200

    Create main.cpp

---
+++ FR_17_01/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main () {
+	
+	int yearAnswer;
+	std::cin >> yearAnswer;
+
+	if (yearAnswer == 2023)
+		std::cout << "tak" << std::endl;
+	else
+		std::cout << "nie" << std::endl;
+
+
+	return 0;
+}
