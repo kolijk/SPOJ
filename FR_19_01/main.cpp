ebbccab343b5d2ab9883eadede32dc8994c327c4
kolijk <kolijk.ggftd@gmail.com>
2026-04-19 11:38:26 +0200

    Create main.cpp

---
+++ FR_19_01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+	
+	int tab[5];
+	std::cin >> tab[0];
+	std::cin >> tab[1];
+	std::cin >> tab[2];
+	std::cin >> tab[3];
+	std::cin >> tab[4];
+
+	std::cout <<tab[0] + tab[1] + tab[2] + tab[3] + tab[4] + 5 <<std::endl;
+
+	return 0;
+}
